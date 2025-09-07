e5e6edde9e1255824b7b0c30473aa9c45e7e3117
sabee676-sudo <sabee676@gmail.com>
2025-09-07 02:51:13 -0600

    Add files via upload

---
+++ convertir_cal.py
@@ -0,0 +1,12 @@
+cal=int(input("Calificasion:"))
+if cal <60:
+    print("Calificacion equivalente (F)")
+elif cal <69: 
+    print("Calificacion equivalente (D)") 
+elif cal <79: 
+    print("Calificacion equivalente (C)") 
+elif cal <89: 
+    print("Calificacion equivalente (B)") 
+elif cal <100: 
+    print("Calificacion equivalente (A)") 
+
